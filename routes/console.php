75318ecc85aba3936c7cdf240987791c17772f7f
birdTechnology2 <birdtechnology66@gmail.com>
2026-09-01 21:34:21 +0300

    Add recurring schedule for check-expired subscriptions and abandoned carts

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Schedule automatic subscription expiration check every 30 minutes
+Schedule::command('subscriptions:check-expired')->everyThirtyMinutes();
+
+// Schedule abandoned carts tracking every 30 minutes
+Schedule::command('abandoned-carts:track')->everyThirtyMinutes();
+
